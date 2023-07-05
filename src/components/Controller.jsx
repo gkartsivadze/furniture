bc2cba79ee7efd598d8fbe17c47698f40bf1fbd6
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-07-05 22:24:43 +0400

    Finished image transition algorithm

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -6,21 +6,22 @@ import { useEffect, useState } from "react"
 library.add(faChevronLeft, faChevronRight)
 
 export default function Controller() {
-    const elements = document.querySelectorAll(".img_container .img");
+    
     const [current,setCurrent] = useState(0);
-    function nextElem() {
-        if (current < elements.length - 1) setCurrent(prev => prev + 1);
-        elements.forEach(elem => elem.classList.remove("active") && elem.classList.add("prev"))
-        elements[current].classList.add("active");
-    }
-    function prevElem() {
-        if (current > 0) setCurrent(prev => prev - 1);
-    }
-    console.log(current);
+    useEffect(() => {
+        const elements = document.querySelectorAll(".img_container .img");
+        elements[current].classList.remove("next");
+        elements.forEach((elem, ind) => {
+            console.log(elem);
+            if (ind < current) elem.classList.add("prev");
+            if (ind > current) elem.classList.add("next")
+            if (ind == current) elem.classList.remove("prev") && elem.classList.remove("next");
+        })
+    }, [current])
     return (
         <div id="controller_container">
-            <button onClick={prevElem}><FontAwesomeIcon icon="fa-solid fa-chevron-left" /></button>
-            <button onClick={nextElem}><FontAwesomeIcon icon="fa-solid fa-chevron-right" /></button>
+            <button onClick={() => current > 0 ? setCurrent(current - 1) : ""}><FontAwesomeIcon icon="fa-solid fa-chevron-left" /></button>
+            <button onClick={() => current < 2 ? setCurrent(current + 1) : ""}><FontAwesomeIcon icon="fa-solid fa-chevron-right" /></button>
         </div>
     )
 }
